ad52d5ce9a32d3dc3057307dac7c5235e62eaa49
Simon1511 <me@simon1511.de>
2024-12-29 13:41:53 +0100

    sm7325-common: manually build libutilscallstack
    Change-Id: Ia3c7083ac77976d7823d65c0c281d66201ece222

--- common.mk
+++ common.mk
@@ -114,6 +114,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider-service.samsung \
     libgrallocusage.vendor \
+    libutilscallstack.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # CAS
